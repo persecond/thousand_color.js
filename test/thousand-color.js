d38f5af8382a23b229380860129a8334c88984ac
Steve <stephentco@gmail.com>
2017-04-18 19:24:55 -0500

    added complementary colors, split up color breakdowns into "classes"

--- test/thousand-color.js
+++ test/thousand-color.js
@@ -1,35 +1,44 @@
 const expect = require("chai").expect;
 const thousandColor = require("../thousand-color");
 
-function analyzeColor(color){
-    return thousandColor.analyzeColor(color);
-}
-
 describe("test color", function(){
     it('should return given color', function(done){
-        expect(analyzeColor("#FFFFFF").hex).to.eql("#FFFFFF");
-        expect(analyzeColor("FFFFFF").hex).to.eql("#FFFFFF");
-        expect(analyzeColor("   FFFFFF  ").hex).to.eql("#FFFFFF");
-        expect(analyzeColor("   #FFFFFF  ").hex).to.eql("#FFFFFF");
-        expect(analyzeColor("#FFF").hex).to.eql("#FFFFFF");
-        expect(analyzeColor("#123").hex).to.eql("#112233");
+        expect(thousandColor.getColor("#FFFFFF").hex).to.eql("#FFFFFF");
+        expect(thousandColor.getColor("FFFFFF").hex).to.eql("#FFFFFF");
+        expect(thousandColor.getColor("   FFFFFF  ").hex).to.eql("#FFFFFF");
+        expect(thousandColor.getColor("   #FFFFFF  ").hex).to.eql("#FFFFFF");
+        expect(thousandColor.getColor("#FFF").hex).to.eql("#FFFFFF");
+        expect(thousandColor.getColor("#123").hex).to.eql("#112233");
         done();
     });
 
     it('should parse RGB breakdown', function(done){
         var rgb;
 
-        rgb = analyzeColor("FF00CC").rgb;
+        rgb = thousandColor.getColor("FF00CC").rgb;
 
-        expect(rgb.R10).to.eql(255);
-        expect(rgb.G10).to.eql(0);
-        expect(rgb.B10).to.eql(204);
+        expect(rgb.getR10()).to.eql(255);
+        expect(rgb.getG10()).to.eql(0);
+        expect(rgb.getB10()).to.eql(204);
 
-        rgb = analyzeColor("F0C").rgb;
+        rgb = thousandColor.getColor("F0C").rgb;
 
-        expect(rgb.R10).to.eql(255);
-        expect(rgb.G10).to.eql(0);
-        expect(rgb.B10).to.eql(204);
+        expect(rgb.getR10()).to.eql(255);
+        expect(rgb.getG10()).to.eql(0);
+        expect(rgb.getB10()).to.eql(204);
+
+        done();
+    });
+
+    it('should parse CMYK breakdown', function(done){
+        var cmyk;
+
+        cmyk = thousandColor.getColor("FF00CC").cmyk;
+
+        expect(cmyk.C).to.eql(0);
+        expect(cmyk.M).to.eql(1);
+        expect(cmyk.Y).to.eql(.2);
+        expect(cmyk.K).to.eql(0);
 
         done();
     });
@@ -37,35 +46,31 @@ describe("test color", function(){
     it('should return expected number of similar colors', function(done){
         const expectedVariation = 5;
 
-        var similarColors, inputColor, inputAnalysis, inputR, inputG, inputB;
+        var similarColors, inputColor, color, inputR, inputG, inputB;
 
         inputColor = "22CC55";
         similarColors = thousandColor.getSimilarColors(inputColor);
         expect(similarColors.length).to.eql(5);
 
-        inputAnalysis = analyzeColor(inputColor)
-        inputR = inputAnalysis.rgb.R10;
-        inputG = inputAnalysis.rgb.G10;
-        inputB = inputAnalysis.rgb.B10;
+        color = thousandColor.getColor(inputColor)
+        inputR = color.getRGB().getR10();
+        inputG = color.getRGB().getG10();
+        inputB = color.getRGB().getB10();
         for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(expectedVariation);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(expectedVariation);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(expectedVariation);
+            var similarColor = similarColors[i];
+            expect(Math.abs(inputR - similarColor.getRGB().getR10())).to.lt(expectedVariation);
+            expect(Math.abs(inputG - similarColor.getRGB().getG10())).to.lt(expectedVariation);
+            expect(Math.abs(inputB - similarColor.getRGB().getB10())).to.lt(expectedVariation);
         }
 
         similarColors = thousandColor.getSimilarColors(inputColor, {maxColors: 10});
         expect(similarColors.length).to.eql(10);
 
-        inputAnalysis = analyzeColor(inputColor)
-        inputR = inputAnalysis.rgb.R10;
-        inputG = inputAnalysis.rgb.G10;
-        inputB = inputAnalysis.rgb.B10;
         for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(expectedVariation);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(expectedVariation);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(expectedVariation);
+            var similarColor = similarColors[i];
+            expect(Math.abs(inputR - similarColor.getRGB().getR10())).to.lt(expectedVariation);
+            expect(Math.abs(inputG - similarColor.getRGB().getG10())).to.lt(expectedVariation);
+            expect(Math.abs(inputB - similarColor.getRGB().getB10())).to.lt(expectedVariation);
         }
 
         done();
@@ -73,39 +78,39 @@ describe("test color", function(){
 
     it('should return similar colors of expected variation', function(done){
 
-        var expectedVariation, similarColors, inputColor, inputAnalysis, inputR, inputG, inputB;
+        var expectedVariation, similarColors, inputColor, color, inputR, inputG, inputB;
 
         inputColor = "22CC55";
-        inputAnalysis = analyzeColor(inputColor)
-        inputR = inputAnalysis.rgb.R10;
-        inputG = inputAnalysis.rgb.G10;
-        inputB = inputAnalysis.rgb.B10;
+        color = thousandColor.getColor(inputColor)
+        inputR = color.getRGB().getR10();
+        inputG = color.getRGB().getG10();
+        inputB = color.getRGB().getB10();
 
         expectedVariation = 5;
         similarColors = thousandColor.getSimilarColors(inputColor, {variation: expectedVariation});
         for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(expectedVariation);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(expectedVariation);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(expectedVariation);
+            var similarColor = similarColors[i];
+            expect(Math.abs(inputR - similarColor.getRGB().getR10())).to.lt(expectedVariation);
+            expect(Math.abs(inputG - similarColor.getRGB().getG10())).to.lt(expectedVariation);
+            expect(Math.abs(inputB - similarColor.getRGB().getB10())).to.lt(expectedVariation);
         }
 
         expectedVariation = 10;
         similarColors = thousandColor.getSimilarColors(inputColor, {variation: expectedVariation});
         for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(expectedVariation);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(expectedVariation);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(expectedVariation);
+            var similarColor = similarColors[i];
+            expect(Math.abs(inputR - similarColor.getRGB().getR10())).to.lt(expectedVariation);
+            expect(Math.abs(inputG - similarColor.getRGB().getG10())).to.lt(expectedVariation);
+            expect(Math.abs(inputB - similarColor.getRGB().getB10())).to.lt(expectedVariation);
         }
 
         expectedVariation = 2;
         similarColors = thousandColor.getSimilarColors(inputColor, {variation: expectedVariation});
         for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(expectedVariation);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(expectedVariation);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(expectedVariation);
+            var similarColor = similarColors[i];
+            expect(Math.abs(inputR - similarColor.getRGB().getR10())).to.lt(expectedVariation);
+            expect(Math.abs(inputG - similarColor.getRGB().getG10())).to.lt(expectedVariation);
+            expect(Math.abs(inputB - similarColor.getRGB().getB10())).to.lt(expectedVariation);
         }
 
         done();
@@ -116,10 +121,6 @@ describe("test color", function(){
         var similarColors, inputColor;
 
         inputColor = "22CC55";
-
-        similarColors = thousandColor.getProportionalColors(inputColor, {maxColors: 1});
-        done();
-        return;
 
         similarColors = thousandColor.getProportionalColors(inputColor);
         expect(similarColors.length).to.eql(5);
@@ -135,42 +136,42 @@ describe("test color", function(){
 
     it('should return proportional colors of expected variation', function(done){
 
-        var expectedVariation, similarColors, inputColor, inputAnalysis, inputR, inputG, inputB;
+        var expectedVariation, proportionalColors, inputColor, color, inputR, inputG, inputB;
 
         inputColor = "22CC55";
-        inputAnalysis = analyzeColor(inputColor);
-        inputR = inputAnalysis.rgb.R10;
-        inputG = inputAnalysis.rgb.G10;
-        inputB = inputAnalysis.rgb.B10;
+        color = thousandColor.getColor(inputColor);
+        inputR = color.getRGB().getR10();
+        inputG = color.getRGB().getG10();
+        inputB = color.getRGB().getB10();
 
         //assert default variation is 10 percent
-        similarColors = thousandColor.getProportionalColors(inputColor);
-        for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(inputR * 0.1);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(inputG * 0.1);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(inputB * 0.1);
+        proportionalColors = thousandColor.getProportionalColors(inputColor);
+        for(var i in proportionalColors){
+            var proportionalColor = proportionalColors[i];
+            expect(Math.abs(inputR - proportionalColor.getRGB().getR10())).to.lt(inputR * 0.1);
+            expect(Math.abs(inputG - proportionalColor.getRGB().getG10())).to.lt(inputG * 0.1);
+            expect(Math.abs(inputB - proportionalColor.getRGB().getB10())).to.lt(inputB * 0.1);
         }
 
         expectedVariation = 20;
-        similarColors = thousandColor.getProportionalColors(inputColor, {maxColors: 10});
-        for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(inputR * 0.2);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(inputG * 0.2);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(inputB * 0.2);
+        proportionalColors = thousandColor.getProportionalColors(inputColor, {maxColors: 10});
+        for(var i in proportionalColors){
+            var proportionalColor = proportionalColors[i];
+            expect(Math.abs(inputR - proportionalColor.getRGB().getR10())).to.lt(inputR * 0.2);
+            expect(Math.abs(inputG - proportionalColor.getRGB().getG10())).to.lt(inputG * 0.2);
+            expect(Math.abs(inputB - proportionalColor.getRGB().getB10())).to.lt(inputB * 0.2);
         }
 
         expectedVariation = 50;
-        similarColors = thousandColor.getProportionalColors(inputColor, {maxColors: 10});
-        for(var i in similarColors){
-            var similarAnalysis = analyzeColor(similarColors[i]);
-            expect(Math.abs(inputR - similarAnalysis.rgb.R10)).to.lt(inputR * 0.5);
-            expect(Math.abs(inputG - similarAnalysis.rgb.G10)).to.lt(inputG * 0.5);
-            expect(Math.abs(inputB - similarAnalysis.rgb.B10)).to.lt(inputB * 0.5);
+        proportionalColors = thousandColor.getProportionalColors(inputColor, {maxColors: 10});
+        for(var i in proportionalColors){
+            var proportionalColor = proportionalColors[i];
+            expect(Math.abs(inputR - proportionalColor.getRGB().getR10())).to.lt(inputR * 0.5);
+            expect(Math.abs(inputG - proportionalColor.getRGB().getG10())).to.lt(inputG * 0.5);
+            expect(Math.abs(inputB - proportionalColor.getRGB().getB10())).to.lt(inputB * 0.5);
         }
 
-        similarColors = thousandColor.getProportionalColors(inputColor, {maxColors: 2});
+        proportionalColors = thousandColor.getProportionalColors(inputColor, {maxColors: 2});
 
         done();
     });
--- thousand-color.js
+++ thousand-color.js
@@ -5,6 +5,186 @@
  */
 (function(){
 
+    // "use strict";
+
+    /**
+     * RGB object
+     * @param {string} hexColor 
+     */
+    var RGB = function(hexColor){
+        var hex = sanitizeHexColor(hexColor);
+        var R = hex.substring(0,2);
+        var G = hex.substring(2,4);
+        var B = hex.substring(4,6);
+        this.R16 = R;
+        this.G16 = G;
+        this.B16 = B;
+        this.R10 = parseInt(R, 16);
+        this.G10 = parseInt(G, 16);
+        this.B10 = parseInt(B, 16);
+    }
+    RGB.prototype.getRHex = function(){
+        return this.R16;
+    }
+    RGB.prototype.getGHex = function(){
+        return this.G16;
+    }
+    RGB.prototype.getBHex = function(){
+        return this.B16;
+    }
+    RGB.prototype.getR10 = function(){
+        return this.R10;
+    }
+    RGB.prototype.getG10 = function(){
+        return this.G10;
+    }
+    RGB.prototype.getB10 = function(){
+        return this.B10;
+    }
+
+    var CMYK = function(rgb){
+        var computedC = 0;
+        var computedM = 0;
+        var computedY = 0;
+        var computedK = 0;
+
+        var R = rgb.getR10();
+        var G = rgb.getG10();
+        var B = rgb.getB10();
+
+        if ( R==null || G==null || B==null ||
+            isNaN(R) || isNaN(G)|| isNaN(B) )
+        {
+            throw 'Please enter numeric RGB values!';
+        }
+        if (R<0 || G<0 || B<0 || R>255 || G>255 || B>255) {
+            throw 'RGB values must be in the range 0 to 255.'
+        }
+
+        // BLACK
+        if (R==0 && G==0 && B==0) {
+            computedK = 1;
+            return [0,0,0,1];
+        }
+
+        computedC = 1 - (R/255);
+        computedM = 1 - (G/255);
+        computedY = 1 - (B/255);
+
+        var minCMY = Math.min(computedC, Math.min(computedM,computedY));
+        computedC = (computedC - minCMY) / (1 - minCMY);
+        computedM = (computedM - minCMY) / (1 - minCMY);
+        computedY = (computedY - minCMY) / (1 - minCMY);
+        computedK = minCMY;
+
+        function to1Decimal(num){
+            return Math.round( num * 10) / 10
+        }
+
+        this.C = to1Decimal(computedC);
+        this.M = to1Decimal(computedM);
+        this.Y = to1Decimal(computedY);
+        this.K = to1Decimal(computedK);
+    }
+    CMYK.prototype.getC = function(){
+        return this.C;
+    }
+    CMYK.prototype.getM = function(){
+        return this.M;
+    }
+    CMYK.prototype.getY = function(){
+        return this.Y;
+    }
+    CMYK.prototype.getK = function(){
+        return this.K;
+    }
+
+    var HSV = function(rgb){
+        var R = rgb.getR10();
+        var G = rgb.getG10();
+        var B = rgb.getB10();
+
+        var max = Math.max(R, G, B), min = Math.min(R, G, B),
+            d = max - min,
+            h,
+            s = (max === 0 ? 0 : d / max),
+            v = max / 255;
+
+        switch (max) {
+            case min: h = 0; break;
+            case R: h = (G - B) + d * (G < B ? 6: 0); h /= 6 * d; break;
+            case G: h = (B - R) + d * 2; h /= 6 * d; break;
+            case B: h = (R - G) + d * 4; h /= 6 * d; break;
+        }
+
+        this.H = h;
+        this.S = s;
+        this.V = v;
+    }
+    HSV.prototype.getH = function(){
+        return this.H;
+    }
+    HSV.prototype.getS = function(){
+        return this.S;
+    }
+    HSV.prototype.getV = function(){
+        return this.V;
+    }
+
+    var Color = function(hexColor){
+        var sanitized = ensureSixHexDigits(hexColor);
+        this.hex = makeHexString(sanitized);
+        this.rgb = new RGB(sanitized);
+        this.cmyk = new CMYK(this.rgb);
+        this.hsv = new HSV(this.rgb);
+    }
+    Color.prototype.getHex = function(){
+        return this.hex;
+    }
+    Color.prototype.getRGB = function(){
+        return this.rgb;
+    }
+    Color.prototype.getCMYK = function(){
+        return this.cmyk;
+    }
+    Color.prototype.getHSV = function(){
+        return this.hsv;
+    }
+    Color.prototype.complement = function(){
+        var hsv = this.getHSV();
+        var h = hsv.getH();
+        var s = hsv.getS();
+        var v = hsv.getV();
+
+        //inverse hue
+        h += .5;
+        if(h > 1){
+            h -= 1;
+        }
+
+        var r, g, b, i, f, p, q, t;
+        if (arguments.length === 1) {
+            s = h.s, v = h.v, h = h.h;
+        }
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+        }
+
+        var complementHex = makeHexStringFromRGB(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255))
+
+        return new Color(complementHex);
+    }
+
     /**
      * Removes any leading "#" or white space from a hex color string.
      * @param {string} hexColor 
@@ -41,23 +221,7 @@
             var B = hex.substring(2,3);
             return R + R + G + G + B + B;
         }else{
-            throw "Invalid hex color length provided, expected 3 or 6 hex digits.";
-        }
-    }
-
-    /**
-     * Returns a breakdown of R, G and B values in decimal.
-     * @param {string} hexColor 
-     */
-    function getRGBDecimal(hexColor){
-        var hex = sanitizeHexColor(hexColor);
-        var R = hex.substring(0,2);
-        var G = hex.substring(2,4);
-        var B = hex.substring(4,6);
-        return {
-            R10: parseInt(R, 16),
-            G10: parseInt(G, 16),
-            B10: parseInt(B, 16)
+            throw "Invalid hex color length provided '" + hex +  "' , expected 3 or 6 hex digits.";
         }
     }
 
@@ -68,7 +232,21 @@
      * @param {number} B 
      */
     function makeHexStringFromRGB(R, G, B){
-        return makeHexString(R.toString(16) + G.toString(16) + B.toString(16));
+
+        function to2Hex(decimal){
+            var string = decimal.toString(16);
+            if(string.length == 1){
+                string = "0" + string;
+            }
+            return string;
+        }
+
+        var RString = to2Hex(R);
+        var GString = to2Hex(G);
+        var BString = to2Hex(B);
+
+
+        return makeHexString(RString + GString + BString);
     }
 
     /**
@@ -108,7 +286,7 @@
         const VARIATION = typeof variation === 'number' ? variation : 5;
 
         const interpretedHex = ensureSixHexDigits(givenHexColor);
-        const RGB = getRGBDecimal(interpretedHex);
+        const rgb = new RGB(interpretedHex);
 
         const maxVariation = Math.floor((VARIATION / 2));
         const minVariation = -1 * maxVariation;
@@ -123,15 +301,15 @@
             var gVariation = getRandomVariation();
             var bVariation = getRandomVariation();
 
-            var R = RGB.R10 + rVariation;
-            var G = RGB.G10 + gVariation;
-            var B = RGB.B10 + bVariation;
+            var R = rgb.getR10() + rVariation;
+            var G = rgb.getG10() + gVariation;
+            var B = rgb.getB10() + bVariation;
 
-            colors[i] = makeHexStringFromRGB(
+            colors[i] = new Color(makeHexStringFromRGB(
                 ensureInHexRange(R),
                 ensureInHexRange(G),
                 ensureInHexRange(B)
-            );
+            ));
         }
         return colors;
 
@@ -157,7 +335,7 @@
         const PERCENT = typeof maxPercent === 'number' ? maxPercent : 10;
 
         const interpretedHex = ensureSixHexDigits(givenHexColor);
-        const RGB = getRGBDecimal(interpretedHex);
+        const rgb = new RGB(interpretedHex);
 
         const max = Math.floor((PERCENT / 2));
         const min = -1 * max;
@@ -170,15 +348,15 @@
 
             var percent = getRandomPercent() / 100;
 
-            var R = RGB.R10 + Math.floor(RGB.R10 * percent);
-            var G = RGB.G10 + Math.floor(RGB.G10 * percent);
-            var B = RGB.B10 + Math.floor(RGB.B10 * percent);
+            var R = rgb.getR10() + Math.floor(rgb.getR10() * percent);
+            var G = rgb.getG10() + Math.floor(rgb.getG10() * percent);
+            var B = rgb.getB10() + Math.floor(rgb.getB10() * percent);
 
-            colors[i] = makeHexStringFromRGB(
+            colors[i] = new Color(makeHexStringFromRGB(
                 ensureInHexRange(R),
                 ensureInHexRange(G),
                 ensureInHexRange(B)
-            );
+            ));
         }
 
         return colors;
@@ -190,26 +368,28 @@
      * @param {string} givenHexColor 
      */
     function analyzeColor(givenHexColor){
-
-        const inputColor = givenHexColor;
-        const interpretedHex = ensureSixHexDigits(inputColor);
-
+        var color = new Color(givenHexColor);
         return {
-            input: inputColor,
-            hex: makeHexString(interpretedHex),
-            rgb: getRGBDecimal(interpretedHex)
+            color: color,
+            complement: color.complement()
         }
+    }
+
+    function getColor(givenHexColor){
+        return new Color(givenHexColor);
     }
 
     //is this node?
     if(typeof exports === 'object'){
         //oh yeah, this is node. cool
+        exports.getColor = getColor;
         exports.analyzeColor = analyzeColor;
         exports.getSimilarColors = getSimilarColors;
         exports.getProportionalColors = getProportionalColors;
     }else{
         //todo make this safer
         thousand_color = {
+            getColor: getColor,
             analyzeColor: analyzeColor,
             getSimilarColors: getSimilarColors,
             getProportionalColors: getProportionalColors
